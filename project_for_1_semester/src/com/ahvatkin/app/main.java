d588e54b3132b780a1e4909581f79544162d27da
Ahvatkin Dmitriy <dmitry92ahv@gmail.com>
2017-10-30 01:53:38 +0600

    input file

---
+++ project_for_1_semester/src/com/ahvatkin/app/main.java
@@ -0,0 +1,10 @@
+package com.ahvatkin.app;
+
+
+import java.io.*;
+public class main {
+
+    public static void main(String args[]) throws IOException {
+        System.out.println(IOFile.read("in.java"));
+    }
+}
